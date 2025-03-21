2a5d6c7531f5f7a0e951278807d60e7edca63dcf
James Gerraty <jrgerraty@waimea.school.nz>
2025-03-21 15:08:40 +1300

    basic gui and appdata

--- src/Main.kt
+++ src/Main.kt
@@ -2,82 +2,112 @@
  * =====================================================================
  * Programming Project for NCEA Level 3, Standard 91906
  * ---------------------------------------------------------------------
- * Project Name:   PROJECT NAME HERE
- * Project Author: James Gerraty
- * GitHub Repo:    https://github.com/waimea-jrgerraty/level-3-programming-assessment
+ * Project Name: PROJECT NAME HERE Project Author: James Gerraty GitHub Repo:
+ * https://github.com/waimea-jrgerraty/level-3-programming-assessment
  * ---------------------------------------------------------------------
- * Notes:
- * The 'map' will be a undirected graph of locations
+ * Notes: The 'map' will be a undirected graph of locations
  * =====================================================================
  */
-
-
-
 import com.formdev.flatlaf.FlatDarkLaf
-import java.awt.*
-import java.awt.event.*
+import java.awt.Color
+import java.awt.Dimension
+import java.awt.Font
+import java.awt.Rectangle
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
 import javax.swing.*
+import kotlin.math.pow
 
+// Constants
+const val HEALTH_MAX_WIDTH = 450
 
-/**
- * Launch the application
- */
-fun main() {
-    FlatDarkLaf.setup()     // Flat, dark look-and-feel
-    MainWindow(App)         // Create and show the UI, using the app model
+// Utilities
+fun sleep(t: Double) {
+    Thread.sleep((t * 1000).toLong())
 }
 
+/** Launch the application */
+fun main() {
+    FlatDarkLaf.setup() // Flat, dark look-and-feel
+    MainWindow(App) // Create and show the UI, using the app model
+}
 
 /**
- * The application class (singleton data-model)
- * This is the place where any application data should be
- * stored, plus any application logic functions
+ * The application class (singleton data-model) This is the place where any application data should
+ * be stored, plus any application logic functions
  */
 object App {
-    // Constants defining any key values
-    val MAX_CLICKS = 10
+    // -- Player stats -- //
+    var level: UByte = 1u
+        private set
 
-    // Data fields
-    var clicks = 0
+    var exp: ULong = 0u
+        private set
 
-    // Application logic functions
-    fun updateClickCount() {
-        clicks++
-        if (clicks > MAX_CLICKS) clicks = MAX_CLICKS
+    val neededExp
+        get() = 1024u + 2.0.pow(level.toInt()).toULong()
+
+    var health = 100.0
+        private set
+
+    val maxHealth
+        get() = 98.0 + (2.0.pow(level.toInt()))
+
+    fun takeDamage(damage: Double) {
+        assert(damage >= 0.0) { "Use the heal method instead of takeDamage" }
+        health = (health - damage).coerceIn(0.0, maxHealth)
+    }
+
+    fun heal(amount: Double) {
+        assert(amount <= 0.0) { "Use the takeDamage method instead of heal" }
+        health = (health + amount).coerceIn(0.0, maxHealth)
+    }
+
+    // -- Persistence -- //
+    fun Marshal(slot: UByte) {
+        /**
+         * Marshal the App data into a file to be loaded later slot will be converted to a string
+         * and added to the file name, so we can have multiple save slots
+         *
+         * project will ask for whatever save slot at the start (maybe from console before the gui
+         * is created? not sure if that's allowed.)
+         */
+        TODO()
+    }
+
+    fun Load(slot: UByte) {
+        TODO()
     }
 }
 
-
 /**
- * Main UI window (view)
- * Defines the UI and responds to events
- * The app model should be passwd as an argument
+ * Main UI window (view) Defines the UI and responds to events The app model should be passwd as an
+ * argument
  */
 class MainWindow(val app: App) : JFrame(), ActionListener {
 
     // Fields to hold the UI elements
-    private lateinit var clicksLabel: JLabel
-    private lateinit var clickButton: JButton
+    private lateinit var display: JTextArea
+    private lateinit var healthLabel: JLabel
+    private lateinit var healthBarFrame: JPanel
+    private lateinit var healthBar: JPanel
+    private lateinit var action: JButton
 
-    /**
-     * Configure the UI and display it
-     */
+    /** Configure the UI and display it */
     init {
-        configureWindow()               // Configure the window
-        addControls()                   // Build the UI
+        configureWindow() // Configure the window
+        addControls() // Build the UI
 
-        setLocationRelativeTo(null)     // Centre the window
-        isVisible = true                // Make it visible
+        setLocationRelativeTo(null) // Centre the window
+        isVisible = true // Make it visible
 
-        updateView()                    // Initialise the UI
+        updateView() // Initialise the UI
     }
 
-    /**
-     * Configure the main window
-     */
+    /** Configure the main window */
     private fun configureWindow() {
         title = "Kotlin Swing GUI Demo"
-        contentPane.preferredSize = Dimension(600, 350)
+        contentPane.preferredSize = Dimension(1200, 900)
         defaultCloseOperation = WindowConstants.EXIT_ON_CLOSE
         isResizable = false
         layout = null
@@ -85,54 +115,67 @@ class MainWindow(val app: App) : JFrame(), ActionListener {
         pack()
     }
 
-    /**
-     * Populate the UI with UI controls
-     */
+    /** Populate the UI with UI controls */
     private fun addControls() {
-        val baseFont = Font(Font.SANS_SERIF, Font.PLAIN, 36)
+        val baseFont = Font(Font.SANS_SERIF, Font.PLAIN, 24)
+        val guiFont = Font(Font.SANS_SERIF, Font.PLAIN, 32)
 
-        clicksLabel = JLabel("CLICK INFO HERE")
-        clicksLabel.horizontalAlignment = SwingConstants.CENTER
-        clicksLabel.bounds = Rectangle(50, 50, 500, 100)
-        clicksLabel.font = baseFont
-        add(clicksLabel)
+        // We will use html tags to control the display text
+        display =
+            JTextArea(
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            )
+        display.lineWrap = true
+        display.isEditable = false
+        display.bounds = Rectangle(50, 50, 1100, 700)
+        display.font = baseFont
+        display.isOpaque = true
+        display.background = Color(50, 50, 60)
+        display.border = BorderFactory.createRaisedSoftBevelBorder()
+        add(display)
 
-        clickButton = JButton("Click Me!")
-        clickButton.bounds = Rectangle(50,200,500,100)
-        clickButton.font = baseFont
-        clickButton.addActionListener(this)     // Handle any clicks
-        add(clickButton)
+        healthLabel = JLabel("Health:")
+        healthLabel.font = guiFont
+        healthLabel.bounds = Rectangle(50, 800, 100, 75)
+        add(healthLabel)
+
+        healthBarFrame = JPanel()
+        healthBarFrame.bounds = Rectangle(175, 800, HEALTH_MAX_WIDTH, 75)
+        healthBarFrame.border = BorderFactory.createRaisedSoftBevelBorder()
+        healthBarFrame.isOpaque = true
+        healthBarFrame.background = Color.BLACK
+        healthBarFrame.layout = null
+        add(healthBarFrame)
+
+        healthBar = JPanel()
+        healthBar.bounds = Rectangle(0, 0, HEALTH_MAX_WIDTH, 75)
+        healthBar.border = BorderFactory.createRaisedSoftBevelBorder()
+        healthBar.isOpaque = true
+        healthBar.background = Color(200, 100, 100)
+        healthBarFrame.add(healthBar)
+
+        action = JButton("Action")
+        action.bounds = Rectangle(200 + HEALTH_MAX_WIDTH, 800, 300, 75)
+        action.font = guiFont
+        action.addActionListener(this) // Handle any clicks
+        add(action)
     }
 
-
-    /**
-     * Update the UI controls based on the current state
-     * of the application model
-     */
-    fun updateView() {
-        if (app.clicks == app.MAX_CLICKS) {
-            clicksLabel.text = "Max clicks reached!"
-            clickButton.isEnabled = false
-        }
-        else {
-            clicksLabel.text = "You clicked ${app.clicks} times"
-            clickButton.isEnabled = true
-        }
+    /** Update the UI controls based on the current state of the application model */
+    private fun updateView() {
+        healthBar.bounds =
+            Rectangle(0, 0, (HEALTH_MAX_WIDTH * (App.health / App.maxHealth)).toInt(), 75)
     }
 
     /**
-     * Handle any UI events (e.g. button clicks)
-     * Usually this involves updating the application model
-     * then refreshing the UI view
+     * Handle any UI events (e.g. button clicks) Usually this involves updating the application
+     * model then refreshing the UI view
      */
     override fun actionPerformed(e: ActionEvent?) {
         when (e?.source) {
-            clickButton -> {
-                app.updateClickCount()
+            action -> {
                 updateView()
             }
         }
     }
-
 }
-
